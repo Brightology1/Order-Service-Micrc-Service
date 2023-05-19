3aa71622fec6f846eba4cfc9ed9295c179c45878
HP 250g7 <brightology2@gmain.com>
2023-05-19 17:03:36 +0100

    OrderService and Repo

--- order-service/src/main/java/com/brightology/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/brightology/orderservice/service/OrderService.java
@@ -1,11 +1,40 @@
 package com.brightology.orderservice.service;
 
+import com.brightology.orderservice.dto.OrderLineItemsDto;
+import com.brightology.orderservice.dto.OrderRequest;
+import com.brightology.orderservice.model.Order;
+import com.brightology.orderservice.model.OrderLineItems;
+import com.brightology.orderservice.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.UUID;
+
 @Service
+@RequiredArgsConstructor
 public class OrderService {
 
+    private final OrderRepository orderRepository;
+    public void placeOrder(OrderRequest orderRequest){
+        Order order = new Order();
+        order.setOrderNumber(UUID.randomUUID().toString());
 
+        List<OrderLineItems> orderLineItems = orderRequest.getOrderLineItemsDtoList()
+                .stream()
+                .map(this::mapToDto)
+                .toList();
+
+        order.setOrderLineItemsList(orderLineItems);
+
+        orderRepository.save(order);
+    }
+
+    private OrderLineItems mapToDto(OrderLineItemsDto orderLineItemsDto){
+        OrderLineItems orderLineItems = new OrderLineItems();
+        orderLineItems.setPrice(orderLineItemsDto.getPrice());
+        orderLineItems.setQuantity(orderLineItemsDto.getQuantity());
+        orderLineItems.setSkuCode(orderLineItemsDto.getSkuCode());
+        return orderLineItems;
+    }
 }
-
-//56.14
